9ad352e4ad42eccb9275bda5fe89d3630eefd374
Julian Zimmermann <54834203+julianzimmermann@users.noreply.github.com>
2021-11-11 15:54:35 +0100

    Add search brand query expander plugins (#4)

---
+++ src/FondOfSpryker/Zed/BrandCompany/Communication/Plugin/BrandExtension/CompanySearchBrandQueryExpanderPlugin.php
@@ -0,0 +1,83 @@
+<?php
+
+namespace FondOfSpryker\Zed\BrandCompany\Communication\Plugin\BrandExtension;
+
+use ArrayObject;
+use FondOfSpryker\Shared\BrandCompany\BrandCompanyConstants;
+use FondOfSpryker\Zed\BrandExtension\Dependency\Plugin\SearchBrandQueryExpanderPluginInterface;
+use Generated\Shared\Transfer\QueryJoinCollectionTransfer;
+use Generated\Shared\Transfer\QueryJoinTransfer;
+use Generated\Shared\Transfer\QueryWhereConditionTransfer;
+use Orm\Zed\Brand\Persistence\Map\FosBrandTableMap;
+use Orm\Zed\BrandCompany\Persistence\Map\FosBrandCompanyTableMap;
+use Propel\Runtime\ActiveQuery\Criteria;
+use Spryker\Zed\Kernel\Communication\AbstractPlugin;
+
+/**
+ * @method \FondOfSpryker\Zed\BrandCompany\Business\BrandCompanyFacadeInterface getFacade()
+ */
+class CompanySearchBrandQueryExpanderPlugin extends AbstractPlugin implements SearchBrandQueryExpanderPluginInterface
+{
+    /**
+     * Specification:
+     * - Returns true if plugin is applicable for given filters.
+     *
+     * @api
+     *
+     * @param array<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
+     *
+     * @return bool
+     */
+    public function isApplicable(array $filterFieldTransfers): bool
+    {
+        foreach ($filterFieldTransfers as $fieldTransfer) {
+            if ($fieldTransfer->getType() === BrandCompanyConstants::FILTER_FIELD_TYPE_ID_CUSTOMER) {
+                return true;
+            }
+        }
+
+        return false;
+    }
+
+    /**
+     * @param array<\Generated\Shared\Transfer\FilterFieldTransfer> $filterFieldTransfers
+     * @param \Generated\Shared\Transfer\QueryJoinCollectionTransfer $queryJoinCollectionTransfer
+     *
+     * @return \Generated\Shared\Transfer\QueryJoinCollectionTransfer
+     */
+    public function expand(
+        array $filterFieldTransfers,
+        QueryJoinCollectionTransfer $queryJoinCollectionTransfer
+    ): QueryJoinCollectionTransfer {
+        $filterFieldTransfer = null;
+
+        foreach ($filterFieldTransfers as $currentFilterFieldTransfer) {
+            if ($currentFilterFieldTransfer->getType() === BrandCompanyConstants::FILTER_FIELD_TYPE_ID_CUSTOMER) {
+                $filterFieldTransfer = $currentFilterFieldTransfer;
+
+                break;
+            }
+        }
+
+        if ($filterFieldTransfer === null || $filterFieldTransfer->getValue() === null) {
+            return $queryJoinCollectionTransfer;
+        }
+
+        $whereConditions = [
+            (new QueryWhereConditionTransfer())
+                ->setValue($filterFieldTransfer->getValue())
+                ->setColumn(FosBrandCompanyTableMap::COL_FK_COMPANY)
+                ->setComparison(Criteria::EQUAL),
+        ];
+
+        $queryJoinCollectionTransfer->addQueryJoin(
+            (new QueryJoinTransfer())
+                ->setJoinType(Criteria::INNER_JOIN)
+                ->setLeft([FosBrandTableMap::COL_ID_BRAND])
+                ->setRight([FosBrandCompanyTableMap::COL_FK_BRAND])
+                ->setWhereConditions(new ArrayObject($whereConditions))
+        );
+
+        return $queryJoinCollectionTransfer;
+    }
+}
